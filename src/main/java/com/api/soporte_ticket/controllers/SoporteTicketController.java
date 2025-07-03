34caa3b292e2d8db6f9dc67e4b5335770557cbfa
benjamin castro <lilbenjalemos@gmail.com>
2025-07-03 15:56:20 -0400

    finalizado

--- src/main/java/com/api/soporte_ticket/controllers/SoporteTicketController.java
+++ src/main/java/com/api/soporte_ticket/controllers/SoporteTicketController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo; 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/soporte")
@@ -39,4 +41,22 @@ public class SoporteTicketController {
         soporteTicketServices.eliminar(id);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/hateoas/{id}")
+    public SoporteTicketDTO obtenerHATEOAS(@PathVariable Integer id) { 
+        SoporteTicketDTO dto = soporteTicketServices.buscar(id);
+        // Agregar enlaces HATEOAS 
+        dto.add(linkTo(methodOn(SoporteTicketController.class).obtenerHATEOAS(id)).withSelfRel()); 
+        dto.add(linkTo(methodOn(SoporteTicketController.class).obtenerTodosHATEOAS()).withRel("todos")); 
+        dto.add(linkTo(methodOn(SoporteTicketController.class).eliminar(id)).withRel("eliminar")); 
+        return dto;
+    }
+    @GetMapping("/hateoas") 
+    public List<SoporteTicketDTO> obtenerTodosHATEOAS() { 
+        List<SoporteTicketDTO> lista = soporteTicketServices.listar(); 
+            for (SoporteTicketDTO dto : lista) { 
+            dto.add(linkTo(methodOn(SoporteTicketController.class).obtenerHATEOAS(dto.getIdSoporte())).withSelfRel()); 
+    } 
+    return lista; 
+} 
 }
--- src/main/java/com/api/soporte_ticket/dto/SoporteTicketDTO.java
+++ src/main/java/com/api/soporte_ticket/dto/SoporteTicketDTO.java
@@ -1,10 +1,12 @@
 package com.api.soporte_ticket.dto;
+
+import org.springframework.hateoas.RepresentationModel;
 import lombok.*;
 import java.sql.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SoporteTicketDTO {
+public class SoporteTicketDTO extends RepresentationModel<SoporteTicketDTO> {
     private Integer idSoporte;
     private Integer idUsuario;
     private String descripcion;
